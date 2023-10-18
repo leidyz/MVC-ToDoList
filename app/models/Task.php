207afd3f0608bab27ab1468a9b8c4e5f5b5b0ed0
PatriPomes <pmanzanastomas@gmail.com>
2023-10-18 22:50:02 +0200

    app/model v2, falta modificar app/controller

--- app/models/Task.php
+++ app/models/Task.php
@@ -1,7 +1,7 @@
 <?php
 
 class Task{
-    private $id;
+    private $id=uniqid();
     private $task_name;
     private $task_description;
     private $start_date;
@@ -9,8 +9,8 @@ class Task{
     private $status;
     private $created_by;
 
-    public function __construct($id, $task_name, $task_description,$start_date, $finish_date, $status, $created_by){  
-        $this->id = $id;
+    public function __construct( $id,$task_name, $task_description,$start_date, $finish_date, $status, $created_by){  
+        $this->id=$id;
         $this->task_name = $task_name;
         $this->task_description = $task_description;
         $this->start_date = $start_date;
@@ -21,24 +21,44 @@ class Task{
     public function getId(){
         return $this->id;
     }
+   
     public function getTaskName(){
         return $this->task_name;
+    }
+    public function setTaskName($task_name){
+        $this->task_name = $task_name;
     }
     public function getTaskDescription(){
     return $this->task_description;
     }
+    public function setTaskDescription($task_description){
+        $this->task_description = $task_description;
+    }
     public function getStartDate(){
         return $this->start_date;
+    }
+    public function setStartDate($start_date){
+        $this->start_date = $start_date;
     }
     public function getFinishDate(){
         return $this->finish_date;
     }
+    public function setFinishDate($finish_date){
+        $this->finish_date = $finish_date;
+    }
     public function getStatus(){
         return $this->status;
+    }
+    public function setStatus($status){
+    $this->status = $status;
     }
     public function getCreatedBy(){
         return $this->created_by;
     }
+    public function setCreatedBy($created_by){
+        $this->created_by = $created_by;
+    }
 
 }
+
 ?>
--- app/models/Task_Model.php
+++ app/models/Task_Model.php
@@ -1,92 +1,156 @@
 <?php
-//$id = uniqid();//genera un id unico
+
+//$task= new Task ($task_name, $task_description,$start_date, $finish_date, $status, $created_by);
 class Task_Model {
     private $jsonFile;
-    private $data;
+    private $task=[];
     
 
-    public function __construct($data) {
+    public function __construct($task) {
         $this->jsonFile=__DIR__.'../app/models/data/DataBase.json';
-        $this->data = $data;
+        $this->task = $task;
     }
 
-    public function getAllTasks() {
-        return $this->data;
+    public function getTask() {
+        return $this->task;
     }
-
-    public function getTaskById($id) {
-        foreach ($this->data as $user) {
-            if (isset($user['task'])) {
-                foreach ($user['task'] as $task) {
-                    if (isset($task['id']) && $task['id'] == $id) {
-                        return $task;
-                    }
-                }
+   
+    public function setJsonFile($jsonFile) {
+        $this->jsonFile=$jsonFile;
+    }
+    public function getJsonFile() {
+        return $this->jsonFile;
+    }
+    private function saveTask()  {
+        $result = file_put_contents($this->jsonFile, json_encode($this->task, JSON_PRETTY_PRINT));
+        
+        if ($result === false) {
+            echo "Error! try again.";
+        }
+    }
+    
+    public function createTask($id, $task_name, $task_description, $start_date, $finish_date, $status, $created_by) {
+       
+        $id = uniqid();
+        $this->task = new Task([
+            'id' => $id,
+            'task_name' => $task_name,
+            'task_description' => $task_description,
+            'start_date' => $start_date,
+            'finish_date'=>$finish_date,
+            'status' => $status,
+            'created_by'=> $created_by]);
+        
+        $this->saveTask();     
+    }    
+        
+    public function deleteTask($id) {
+        foreach ($this->task as $key => $task) {
+            if ($task['id'] == $id) {
+                unset($this->task[$key]); 
+                $this->saveTask(); 
+                return true; 
             }
         }
-        return null;
-    }
+            return false; 
+    }    
 
-    public function addTask($id, $task) {
-      
-        foreach ($this->data as &$user) {
-            if ($user[$id] == $id) {
-                if (!isset($user['task'])) {
-                    $user['task'] = [];
-                }
-                $user['task'][] = $task;
-                return true;
-            }
-        }
-        return false;
-    }
+    public function edit($id,$task_name,$task_description,$start_date,$finish_date,$status,$created_by){
+        foreach ($this->task as $key => $task) {
+            if ($task['id'] == $id) {
+            
+            $task['task_name'] = $task_name;
+            $task['task_description'] =$task_description;
+            $task['start_date'] = $start_date;
+            $task['finish_date']=$finish_date;
+            $task['status']= $status;
+            $task['created_by']= $created_by;
 
-    public function updateTask($id, $taskId, $updatedTask) {
-        foreach ($this->data as &$user) {
-            if ($user[$id] == $id && isset($user['task'])) {
-                foreach ($user['task'] as &$task) {
-                    if (isset($task[$id]) && $task[$id] == $taskId) {
-                        $task = array_merge($task, $updatedTask);
-                        return true;
-                    }
+            foreach ($this->task as $key => $value) {
+                if ($value['id'] === $id) {
+                    $this->task[$key] = $task;
+                    break;
                 }
             }
-        }
-        return false;
-    }
-
-    public function deleteTask($id, $taskId) {
-        foreach ($this->data as &$user) {
-            if ($user[$id] == $id && isset($user['task'])) {
-                foreach ($user['task'] as $key => $task) {
-                    if (isset($task[$id]) && $task[$id] == $taskId) {
-                        unset($user['task'][$key]);
-                        return true;
-                    }
-                }
+            $this->saveTask();
             }
-        }
-        return false;
+        }     
     }
-
-    // Función para obtener todas las tareas con su estado, hora de inicio y finalización, y usuario creador
-    public function getAllTasksWithDetails() {
-        $tasksWithDetails = [];
-        foreach ($this->data as $user) {
-            if (isset($user['task'])) {
-                foreach ($user['task'] as $task) {
-                    $taskDetails = [
-                        'task_name' => $task['task_name'],
-                        'task_description' => $task['task_description'],
-                        'start_date' => $task['start_date'],
-                        'finish_date' => $task['finish_date'],
-                        'status' => $task['status'],
-                        'created_by' => $task['created_by'],
-                    ];
-                    $tasksWithDetails[] = $taskDetails;
-                }
-            }
-        }
-        return $tasksWithDetails;
-    }
+    public function showTask(){}
 }
+
+    // public function getTaskById($id) {
+    //     foreach ($this->task as $user) {
+    //         if (isset($user['task'])) {
+    //             foreach ($user['task'] as $task) {
+    //                 if (isset($task['id']) && $task['id'] == $id) {
+    //                     return $task;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
+
+    // public function addTask($id, $task) {
+      
+    //     foreach ($this->data as &$user) {
+    //         if ($user[$id] == $id) {
+    //             if (!isset($user['task'])) {
+    //                 $user['task'] = [];
+    //             }
+    //             $user['task'][] = $task;
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // public function updateTask($id, $taskId, $updatedTask) {
+    //     foreach ($this->data as &$user) {
+    //         if ($user[$id] == $id && isset($user['task'])) {
+    //             foreach ($user['task'] as &$task) {
+    //                 if (isset($task[$id]) && $task[$id] == $taskId) {
+    //                     $task = array_merge($task, $updatedTask);
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // public function deleteTask($id, $taskId) {
+    //     foreach ($this->data as &$user) {
+    //         if ($user[$id] == $id && isset($user['task'])) {
+    //             foreach ($user['task'] as $key => $task) {
+    //                 if (isset($task[$id]) && $task[$id] == $taskId) {
+    //                     unset($user['task'][$key]);
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // // Función para obtener todas las tareas con su estado, hora de inicio y finalización, y usuario creador
+    // public function getAllTasksWithDetails() {
+    //     $tasksWithDetails = [];
+    //     foreach ($this->data as $user) {
+    //         if (isset($user['task'])) {
+    //             foreach ($user['task'] as $task) {
+    //                 $taskDetails = [
+    //                     'task_name' => $task['task_name'],
+    //                     'task_description' => $task['task_description'],
+    //                     'start_date' => $task['start_date'],
+    //                     'finish_date' => $task['finish_date'],
+    //                     'status' => $task['status'],
+    //                     'created_by' => $task['created_by'],
+    //                 ];
+    //                 $tasksWithDetails[] = $taskDetails;
+    //             }
+    //         }
+    //     }
+    //     return $tasksWithDetails;
+    // }
