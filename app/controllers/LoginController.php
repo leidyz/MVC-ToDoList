5ea8e70f30f410c7fefe824d839f9ef57d342a4d
leidyz <leidy.zhang98@gmail.com>
2023-10-23 17:20:31 +0200

    added register model and view

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,7 +3,7 @@
 session_start();
 require "../app/models/login.php";
 class LoginController{
-    public function index(){
+    public function indexAction(){
         if(isset($_SESSION['login']))
         header('location:' .urlsite);
         require 'views/LoginView.php';
