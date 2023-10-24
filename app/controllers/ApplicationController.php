6951b8411205dfda4c58cbd1eecc29732e1302c0
leidyz <leidy.zhang98@gmail.com>
2023-10-24 10:19:39 +0200

    create route and controller for registerUser

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once  '../app/models/RegisterUser.php';
 /**
  * Base controller for the application.
  * Add general things in this controller.
@@ -7,5 +8,13 @@
 class ApplicationController extends Controller 
 {
 	
+
+public function RegisterUserAction(){
+    if(isset($_POST['submit'])){
+        $user = new RegisterUser($_POST['username'],$_POST['password']);
+    }
+}
+
+
 
 }
