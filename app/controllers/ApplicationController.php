c6b984c12c2f0ba9b32323b2739ccb615c613a82
leidyz <leidy.zhang98@gmail.com>
2023-10-24 10:52:51 +0200

    Creation of login functions in controller

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -1,19 +1,45 @@
 <?php
 
-require_once  '../app/models/RegisterUser.php';
+require_once  '../app/models/RegisterUserModel.php';
+require_once  '../app/models/LoginUserModel.php';
 /**
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
-{
+class ApplicationController extends Controller {
 	
 
-public function RegisterUserAction(){
-    if(isset($_POST['submit'])){
-        $user = new RegisterUser($_POST['username'],$_POST['password']);
+    public function RegisterUserAction(){
+        if(isset($_POST['submit'])){
+            $user = new RegisterUser($_POST['username'],$_POST['password']);
+        }
     }
+
+
+    public function LoginUserAction(){
+        if(isset($_POST['submit'])){
+            $user = new LoginUser($_POST['username'],$_POST['password']);
+        }
+    }
+
+    //para activar el de acccountview
+    public function LogoutUserAction(){
+        session_start();
+        if(!isset($_SESSION['user'])){
+        header("location: LoginView.phtml"); exit();
+
+        }
+
+        if(isset($_GET['logout'])){
+        unset($_SESSION['user']);
+        header("location: LoginView.phtml"); exit();
+        }
+    }
+
 }
+
+
+?>
 
 
 
